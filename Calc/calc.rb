59ddd234d9b51c148e583281901fea1aa40ec536
LiviaMartinss <lilivitt001@gmail.com>
2023-10-21 04:20:40 -0300

    some calculations added

---
+++ Calc/calc.rb
@@ -0,0 +1,11 @@
+print "Digite o primeiro nímero inteiro: "
+
+firstNumber = gets.chomp.to_i
+
+print "Digite o segundo número inteiro: "
+
+secondNumber = gets.chomp.to_i
+
+addition = firstNumber + secondNumber
+
+puts "O resultado da adição é #{addition}"
---
+++ Calc/operationsWithNumbers.rb
@@ -0,0 +1,29 @@
+print " Digite seu nome :) ! "
+
+name = gets.chomp
+
+print "Digite sua idade: "
+
+age = gets.chomp.to_i
+
+puts "O seu nome é #{name} né? e sua idade #{age}, shoow"
+
+puts "ah mais uma coisa"
+
+print "digite um de dois numeros inteiros "
+
+numberOne = gets.chomp.to_i
+
+print "digite outro numeros inteiros "
+
+numberTwo = gets.chomp.to_i
+
+soma = numberOne + numberTwo
+
+subtracao = numberOne - numberTwo
+
+divisao = numberOne / numberTwo
+
+multiplicacao = numberOne * numberTwo
+
+puts "prontinhoo, aqui a sua soma #{soma}, subtração #{subtracao}, divisão #{divisao} e multiplicação #{multiplicacao}"
